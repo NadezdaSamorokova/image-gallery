93d9afb8bfeffeabf979954de78d3359e4c7be18
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2021-06-16 23:02:48 +0200

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,29 @@
 const openPopup = document.querySelector('.profile-info__edit-button');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__close-icon');
-
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
-}
-
 const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__input-name');
-const jobInput = document.querySelector('.popup__input-occupation');
+const nameInput = document.querySelector('input[name=name]');
+const jobInput = document.querySelector('input[name=occupation]');
 const profileName = document.querySelector('.profile-info__name');
 const profileOccupation = document.querySelector('.profile-info__occupation');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-
-    profileName.textContent = nameInput.value;
-    profileOccupation.textContent = jobInput.value;
-    togglePopup();
+function openedPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileOccupation.textContent
 }
 
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
+function formCloseHandler () {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileOccupation.textContent = jobInput.value;
+    popup.classList.toggle('popup_opened');
+}
+
+openPopup.addEventListener('click', openedPopup);
+closePopup.addEventListener('click', formCloseHandler);
 formElement.addEventListener('submit', formSubmitHandler); 

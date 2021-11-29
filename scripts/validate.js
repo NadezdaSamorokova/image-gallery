e7e377e4eb2f447b91df0b92cb91eff8e1885c6a
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2021-11-29 20:57:29 +0100

    mesto 6

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,4 @@
+//объект с классами и селекторами
 const config = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
@@ -7,22 +8,18 @@ const config = {
     errorClass: 'popup__error_visible'
 };
 
-function enableValidation () {
-    const forms = Array.from(document.querySelectorAll('.popup__form'));
+//функция слушатель валидации всех форм в проекте
+function enableValidation (config) {
+    const forms = Array.from(document.querySelectorAll(config.formSelector));
     
     forms.forEach(addListenersToForm);
 }
 
+//функция слушатель всех полей ввода в проекте
 function addListenersToForm(form) {
-    const inputs = Array.from(form.querySelectorAll('.popup__input'));
+    const inputs = Array.from(form.querySelectorAll(config.inputSelector));
 
     inputs.forEach(addListenersToInput);
-
-
-
-
-
-
 
     form.addEventListener('submit', handleSubmit);
     form.addEventListener('input', handleFormInput);
@@ -35,16 +32,18 @@ function handleFormInput (evt) {
     setSubmitButtonState(form);
 }
 
+// Функция добавления и удаления ошибки в полях ввода
 function setSubmitButtonState (form) {
-    const button = form.querySelector('.popup__submit-button');
+    const button = form.querySelector(config.submitButtonSelector);
     button.disabled = !form.checkValidity();
-    button.classList.toggle('popup__submit-button_disabled', !form.checkValidity());
+    button.classList.toggle(config.inactiveButtonClass, !form.checkValidity());
 }
 
+//функция обработки каждого элемента поля ввода в формах
 function handleSubmit (evt) {
     evt.preventDefault();
     const form = evt.target;
-    const data = Array.from(form.querySelectorAll('.popup__input')).reduce((input)=>{
+    const data = Array.from(form.querySelectorAll(config.inputSelector)).reduce((input, sum)=>{
         sum[input.name] = input.value;
         return sum;
     }, {})
@@ -52,48 +51,49 @@ function handleSubmit (evt) {
     return data;
 }
 
-
-
-
-
-
-
-
+//функция слущатель для поля ввода
 function addListenersToInput(input) {
     input.addEventListener('input', handleFieldValidation);
 }
 
+//функция проверки поя ввода на ошибки
 function handleFieldValidation (evt) {
     const element = evt.target;
     element.setCustomValidity('');
     const errorContainer = document.querySelector(`#${element.id}-error`);
 
+    //вызовы функций валидации параметров вводимой информации
     validateLength(element);
     validateRequired(element);
     validateLink(element);
 
     errorContainer.textContent = element.validationMessage;
 
-    element.classList.toggle('popup__input_error', !element.validity.valid); 
-    errorContainer.classList.toggle('popup__error_visible', !element.validity.valid)
+    //навешивание/снятие сообщения об ошибке 
+    element.classList.toggle(config.inputErrorClass, !element.validity.valid); 
+    errorContainer.classList.toggle(config.errorClass, !element.validity.valid)
 }
 
+//функция валидации длинны вводимой информации
 function validateLength(inputElement) {
     if (inputElement.validity.tooShort || inputElement.validity.tooLong) {
         inputElement.setCustomValidity('Минимальное количество символов: 2. Длина текста сейчас: 1 символ');
     }
 }
 
+//функция валидации необходимости вводимой информации
 function validateRequired(inputElement) {
     if (inputElement.validity.valueMissing) {
         inputElement.setCustomValidity('Вы пропустили это поле');
     }
 }
 
+//функция валидации типа вводимой информации
 function validateLink(inputElement) {
     if (inputElement.type === 'url' && inputElement.validity.typeMismatch) {
         inputElement.setCustomValidity('Введите адрес сайта');
     }
 }
 
-enableValidation();
+//вызов функции валидации всех фрм проекта
+enableValidation(config);
